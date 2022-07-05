052684dd6e405e035f354a18ed4306f0336c8e7a
Ian King <ian.antking@gmail.com>
2022-07-05 11:46:26 +0100

    add keybow module

--- hue-mote/app.py
+++ hue-mote/app.py
@@ -4,6 +4,9 @@ class App:
 
 if __name__ == '__main__':
   import argparse
+  import keybow
+
+  keybow.setup(keybow.MINI)
 
   parser = argparse.ArgumentParser(description='Keybow remote for hue lights')
   parser.add_argument('--room', metavar='r', required=True, type=str, help='The name of the room for hue-mote to control')
@@ -12,4 +15,12 @@ if __name__ == '__main__':
 
   app = App(args.room)
 
-  print(app.room)
+  @keybow.on()
+  def handle_key(index, state):
+    if state:
+        keybow.set_led(index, 255, 0, 0)
+    else:
+        keybow.set_led(index, 0, 0, 0)
+
+  while True:
+    keybow.show()
